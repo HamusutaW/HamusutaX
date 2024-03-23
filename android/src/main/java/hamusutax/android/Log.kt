5a2d255b91eb5358366b6c3e079f87790314a199
Zhang Zihan <zhangzihanzhangzihan@live.com>
2024-03-23 21:03:16 +0800

    修改 reportLog

--- android/src/main/java/hamusutax/android/Log.kt
+++ android/src/main/java/hamusutax/android/Log.kt
@@ -3,8 +3,8 @@ package hamusutax.android
 
 import android.util.Log
 
-fun Throwable.reportLog(message: String? = this.message, tag: String = this::class.simpleName ?: "<UnknownException>") =
+fun Throwable.reportLog(msg: String? = null, tag: String = this::class.simpleName ?: "<UnknownException>") =
     Log.e(tag, buildString {
-        append("${this@reportLog::class.qualifiedName}: $localizedMessage")
-        message?.let { append(" | $it") }
+        msg?.let { append("$it | ") }
+        append(message)
     })
